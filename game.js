7952d60d75e78bc781274c53451d90041a3fafec
Yui Guji <nikitunzod@gmail.com>
2022-07-25 21:18:53 +0300

    Modifying button

--- game.js
+++ game.js
@@ -4,81 +4,88 @@ const gameExecute = () => {
 
    const gameVar = () => {
 
-   const variables = ['rock', 'paper', 'scissors']
-   const computerChoice = variables[Math.floor(Math.random() * variables.length)]
-   
-   switch (computerChoice) {
+      const buttons = document.querySelectorAll('button')
+      buttons.forEach((button) => {
+         button.addEventListener('click', () => {
 
-      case 'paper':
-
-         if (playerChoice == 'rock') {
-            console.log('Computer wins! Paper beats rock!')
-            computerScore += 1
-         }
-
-         else if (playerChoice == 'scissors') {
-            console.log('You win! Scissors beat paper!')
-            playerScore += 1
-         }
-         else {
-            console.log('TIE!')
-         }
-         break
-
-      case 'rock':
-
-         if (playerChoice == 'scissors') {
-            console.log('Computer wins! Rock beats scissors!')
-            computerScore += 1
-         }
-
-         else if (playerChoice == 'paper') {
-            console.log('You win! Paper beats rock!')
-            playerScore += 1
-         }
-         else {
-            console.log('TIE!')
-         }
-         break
-      
-      case 'scissors':
-
-         if (playerChoice == 'paper') {
-            console.log('Computer wins! Scissors beat paper!')
-            computerScore += 1
-         }
-
-         else if (playerChoice == 'rock') {
-            console.log('You win! Rock beats scissors!')
-            playerScore += 1
-         }
-         else {
-            console.log('TIE!')
-         }
-         break
+            const playerChoice = button.textContent.toLowerCase()
+            const variables = ['rock', 'paper', 'scissors']
+            const computerChoice = variables[Math.floor(Math.random() * variables.length)]
          
-   }
-}
-
+            switch (computerChoice) {
+      
+               case 'paper':
+      
+                  if (playerChoice == 'rock') {
+                     console.log('Computer wins! Paper beats Rock!')
+                     computerScore += 1
+                  }
+      
+                  else if (playerChoice == 'scissors') {
+                     console.log('You win! Scissors beat Paper!')
+                     playerScore += 1
+                  }
+                  else {
+                     console.log('TIE!')
+                  }
+                  break
+      
+               case 'rock':
+      
+                  if (playerChoice == 'scissors') {
+                     console.log('Computer wins! Rock beats Scissors!')
+                     computerScore += 1
+                  }
+      
+                  else if (playerChoice == 'paper') {
+                     console.log('You win! Paper beats Rock!')
+                     playerScore += 1
+                  }
+                  else {
+                     console.log('TIE!')
+                  }
+                  break
+               
+               case 'scissors':
+      
+                  if (playerChoice == 'paper') {
+                     console.log('Computer wins! Scissors beat Paper!')
+                     computerScore += 1
+                  }
+      
+                  else if (playerChoice == 'rock') {
+                     console.log('You win! Rock beats Scissors!')
+                     playerScore += 1
+                  }
+                  else {
+                     console.log('TIE!')
+                  }
+                  break
+                  
+               }
+            })
+         })
+      
+   };
    gameVar()
 
    // for (let i = 0; i < 5; i++) {
    //    gameVar()
    // }
 
-   console.log(`The player's score is: ${playerScore}\nThe computer's score is: ${computerScore}`)
+   // console.log(`The player's score is: ${playerScore}\nThe computer's score is: ${computerScore}`)
 
-   if (computerScore > playerScore) {
-      console.log('Computer wins!')
-   }
+   // if (computerScore > playerScore) {
+   //    console.log('Computer wins!')
+   // }
 
-   else if (computerScore == playerScore) {
-      console.log('TIE!')
-   }
+   // else if (computerScore == playerScore) {
+   //    console.log('TIE!')
+   // }
 
-   else {
-      console.log('Player wins')
-   }
+   // else {
+   //    console.log('Player wins')
+   // }
 
 }
 
